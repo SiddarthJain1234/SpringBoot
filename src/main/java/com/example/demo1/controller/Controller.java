048418758c2b83c85a462361e53c0f39cc002938
sidhu8300 <jainsiddarth83@gmail.com>
2022-06-16 13:10:58 +0530

    changed delete

--- src/main/java/com/example/demo1/controller/Controller.java
+++ src/main/java/com/example/demo1/controller/Controller.java
@@ -1,4 +1,5 @@
 package com.example.demo1.controller;
+
 import com.example.demo1.model.*;
 
 import java.util.ArrayList;
@@ -17,51 +18,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class Controller {
 
-	List<Employee> list=new ArrayList<Employee>();
-	
-	
+	List<Employee> list = new ArrayList<Employee>();
+
 	public Controller() {
-	list.add(new Employee(1,"Atul","IT"));
-	list.add(new Employee(2,"Aditya","IT"));
-	list.add(new Employee(3,"Harsh","IT"));
+		list.add(new Employee(1, "Atul", "IT"));
+		list.add(new Employee(2, "Aditya", "IT"));
+		list.add(new Employee(3, "Harsh", "IT"));
 
 	}
-	
+
 	@GetMapping("/")
 	public List<Employee> hello() {
 		return list;
 	}
-	
+
 	@PostMapping("/")
 	public List<Employee> postMethod(@RequestBody Employee e) {
 		list.add(e);
 		return list;
 	}
-	
+
 	@GetMapping("/{id}")
 	public Employee getById(@PathVariable int id) {
-		return list.get(id-1);
+		return list.get(id - 1);
 	}
-	
+
 	@PutMapping("/")
-	public List<Employee> putMethod(@RequestBody Employee e){
-		
-		for(int i=0; i<list.size(); i++) {
-			if(list.get(i).getId()==e.getId()) {
+	public List<Employee> putMethod(@RequestBody Employee e) {
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getId() == e.getId()) {
 				list.set(i, e);
 				return list;
 			}
 		}
-		
+
 		list.add(e);
 		return list;
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public List<Employee> deleteMethod(@PathVariable int id){
-		list.remove(id-1);
+	public List<Employee> deleteMethod(@PathVariable int id) {
+		list.set(id-1, null);
 		return list;
 	}
-	
-	
+
 }
